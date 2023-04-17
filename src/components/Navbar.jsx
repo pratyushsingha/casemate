d431f847a2a2ea213fbf61eee11f6135f2d205d8
pratyushsingha <pratyushsingha83@gmail.com>
2023-04-17 23:22:48 +0530

    uppercase & lowercase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from '/favicon.ico'
 
 const Navbar = (props) => {
   return (
@@ -6,9 +7,7 @@ const Navbar = (props) => {
       <header className="text-gray-600 body-font">
   <div className="container mx-auto flex justify-center flex-wrap p-5 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
+    <img className='bg-yellow-500 text-yellow-500' src={logo} alt="Logo" />
       <span className="ml-3 text-xl">{props.title}</span>
     </a>
    
