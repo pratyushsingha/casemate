37f7845de7f61c0d7b09c99aab360eea0b85d0ae
pratyushsingha <pratyushsingha83@gmail.com>
2023-04-18 00:01:51 +0530

    css added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = (props) => {
       <header className="text-gray-600 body-font">
   <div className="container mx-auto flex justify-center flex-wrap p-5 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-    <img className='bg-yellow-500 text-yellow-500' src={logo} alt="Logo" />
+    {/* <img src={logo} alt="Logo" /> */}
       <span className="ml-3 text-xl">{props.title}</span>
     </a>
    
--- src/components/TextEdit.jsx
+++ src/components/TextEdit.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import hi from '/hi.gif';
 
 const TextEdit = (props) => {
   // for uppercase converter
@@ -24,6 +25,9 @@ const TextEdit = (props) => {
   return (
     <>
       <div className="message md:mx-96">
+        <div className='flex justify-center items-center'>
+          <img className='w-16 h-16 rounded-2xl' src={hi} alt="" />
+        </div>
         <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{props.title}</label>
         <div className="flex space-x-4">
           <textarea id="message" rows="4" onChange={handleOnChange} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={props.placeholder}></textarea>
@@ -37,17 +41,17 @@ const TextEdit = (props) => {
         <div>Word count:{text.split(" ").length} </div>
         <div>Line count:{text.split(/\r\n|\r|\n/).length}</div>
 
-
       </div>
       <div className="buttonMiddle flex justify-center items-center">
         <div className="flex justify-center items-center my-3">
-          <button type="button" className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900" onClick={handleUpClick}>{props.upperCase}</button>
+          <button type="button" className="focus:outline-none text-md text-white bg-purple-700 hover:bg-purple-900 focus:ring-4 focus:ring-purple-600 font-medium rounded-lg px-5 py-2.5 mr-2 mb-2 dark:focus:ring-purple-700" onClick={handleUpClick}>{props.upperCase}</button>
         </div>
 
         <div className="my-3">
-          <button type="button" className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900" onClick={handleLowerClick}>{props.lowerCase}</button>
+          <button type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-900 focus:ring-4 focus:ring-purple-600 font-medium rounded-lg text-md px-5 py-2.5 mr-2 mb-2 dark:focus:ring-purple-700" onClick={handleLowerClick}>{props.lowerCase}</button>
         </div>
       </div>
+
 
     </>
   )
